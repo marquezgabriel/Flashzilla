dc9ac09b9a24b760cec3d20b4d7fb6572f018dba
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-15 12:45:24 -0400

    Day 86 - Making vibrations with UINotificationFeedbackGenerator and Core Haptics

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,59 +5,69 @@
 //  Created by Gabriel Marquez on 2022-08-15.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
-    
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged{ value in
-                offset = value.translation
-            }
-            .onEnded{ _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-                
-            }
+        Text("Hello, world!")
+            // .padding()
+            // .onTapGesture(perform: simpleSuccess)
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-        
-        VStack {
-            Text("Hello, world!")
-                .onTapGesture {
-                    print("Text tapped!")
-                }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
-        .simultaneousGesture(
-            TapGesture()
-                .onEnded {
-                    print("VStack tapped")
-                }
-        )
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+                
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime:0)
+        } catch {
+            print("Failed to play patter \(error.localizedDescription)")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 11 Pro")
     }
 }
