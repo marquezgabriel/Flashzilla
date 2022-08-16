8fffbb2aa3a4956e2a3ca547346d4e57c8f7772e
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-16 14:17:35 -0400

    Complete day 87

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,23 +5,13 @@
 //  Created by Gabriel Marquez on 2022-08-15.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
-
     var body: some View {
-        Text("Hello, World!")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-
-                counter += 1
-            }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
