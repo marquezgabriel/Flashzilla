70176411b41306c7575d0d25617aacef3b5c90e3
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-19 11:30:44 -0400

    Complete day 90

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -18,13 +18,16 @@ extension View {
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
-    @State private var cards = Array(repeating: Card.example, count: 10)
+    // @State private var cards = Array(repeating: Card.example, count: 10)
+    @State private var cards = [Card]()
     
     @State private var timeRemaning = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     @Environment(\.scenePhase) var scenePhase
     @State private var isActive = true
+    
+    @State private var showingEditScreen = false
     
     var body: some View {
         ZStack {
@@ -63,6 +66,27 @@ struct ContentView: View {
                         .clipShape(Capsule())
                 }
             }
+            
+            VStack {
+                HStack {
+                    Spacer()
+
+                    Button {
+                        showingEditScreen = true
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                }
+
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
+            
             if differentiateWithoutColor || voiceOverEnabled {
                 VStack {
                     Spacer()
@@ -119,6 +143,16 @@ struct ContentView: View {
                 isActive = false
             }
         }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards, content: EditCards.init)
+        .onAppear(perform: resetCards)
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
     }
     
     func removeCard(at index: Int) {
@@ -132,9 +166,9 @@ struct ContentView: View {
     }
     
     func resetCards () {
-        cards = Array<Card>(repeating: Card.example, count: 10)
         timeRemaning = 100
         isActive = true
+        loadData()
     }
     
 }
